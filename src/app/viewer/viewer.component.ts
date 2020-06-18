7ee684c3ac8d893a3adf41b5fd1abeb2b7c5007f
legendhl <legendhl@gmail.com>
2020-06-18 19:30:42 +0800

    MOD: modify image style

--- src/app/viewer/viewer.component.ts
+++ src/app/viewer/viewer.component.ts
@@ -1,17 +1,17 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef, ViewChild, ElementRef } from '@angular/core';
 import { ipcRenderer, remote } from 'electron';
 
 @Component({
   selector: 'app-viewer',
   templateUrl: './viewer.component.html',
   styleUrls: ['./viewer.component.scss'],
-  // changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ViewerComponent implements OnInit {
+  @ViewChild('img') imgElement: ElementRef<HTMLImageElement>;
   showOpenFileBtn: boolean;
   imgSrc: string;
 
-  constructor() {
+  constructor(private ref: ChangeDetectorRef) {
   }
 
   ngOnInit(): void {
@@ -23,6 +23,7 @@ export class ViewerComponent implements OnInit {
         if (images.length > 0) {
           this.showOpenFileBtn = false;
           this.imgSrc = `file://${images[current]}`;
+          this.ref.detectChanges();
         } else {
           console.log('no image opened');
         }
@@ -35,15 +36,30 @@ export class ViewerComponent implements OnInit {
   }
 
   previous(): void {
-    //
+    console.log('previous');
   }
 
   next(): void {
-    //
+    console.log('next');
   }
 
   maxWindow(): void {}
-  resize(): void {}
+  resize(): void {
+    // console.log(document.body, this.imgElement);
+    const body = document.body;
+    const { offsetWidth, offsetHeight } = body;
+    // console.log(offsetWidth, offsetHeight);
+    const img = this.imgElement.nativeElement;
+    const { offsetWidth: imgWidth, offsetHeight: imgHeight } = img;
+    // console.log(img, imgWidth, imgHeight);
+    if (offsetWidth * imgHeight > imgWidth * offsetHeight) {
+      // 窗口更宽
+      img.style.transform = `scale(${offsetHeight / imgHeight})`;
+    } else {
+      // 图片更宽
+      img.style.transform = `scale(${offsetWidth / imgWidth})`;
+    }
+  }
   resetSize(): void {}
   realSize(): void {}
   initParams(): void {}
