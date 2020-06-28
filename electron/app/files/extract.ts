dba3b8e49ffbfd195fe2b1d4d5de5268995ab58c
legendhl <legendhl@gmail.com>
2020-06-28 12:08:54 +0800

    MOD: replace jszip with unzipper

--- electron/app/files/extract.ts
+++ electron/app/files/extract.ts
@@ -1,55 +1,25 @@
 import * as fs from 'fs';
 import * as path from 'path';
-import * as JSZip from 'jszip';
+import * as unzipper from 'unzipper';
 import { app } from 'electron';
+import * as rimraf from 'rimraf';
 
 export function extractFiles(filepath: string): Promise<any> {
   const tempDir = app.getPath('temp');
   const imageTempDir = path.join(tempDir, 'comic-viewer');
   const imageCacheTempDir = path.join(imageTempDir, '__MACOSX');
 
+  rimraf.sync(imageTempDir);
   if (!fs.existsSync(imageTempDir)) {
     fs.mkdirSync(imageTempDir);
   }
   if (!fs.existsSync(imageCacheTempDir)) {
     fs.mkdirSync(imageCacheTempDir);
   }
-  return new Promise((resolve, reject) => {
-    fs.readFile(filepath, (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(data);
-      }
-    });
-  })
-    .then((data: Buffer) => JSZip.loadAsync(data))
-    .then((zip) => extract(zip.files, imageTempDir))
+  return fs
+    .createReadStream(filepath)
+    .pipe(unzipper.Extract({ path: imageTempDir }))
+    .promise()
+    .catch((e) => {})
     .then(() => imageTempDir);
-}
-
-function extract(files: { [key: string]: JSZip.JSZipObject }, saveDir: string): Promise<any> {
-  return Promise.all(
-    Object.keys(files).map((filename) => {
-      const fileObj = files[filename];
-      if (fileObj.dir) {
-        // 忽略子文件夹
-        return Promise.resolve();
-      }
-      const fullpath = path.join(saveDir, filename);
-      // nodebuffer: the result will be a nodejs Buffer. This requires nodejs.
-      return fileObj.async('nodebuffer').then(
-        (content) =>
-          new Promise((resolve, reject) => {
-            fs.writeFile(fullpath, content, (err) => {
-              if (err) {
-                reject(err);
-              } else {
-                resolve();
-              }
-            });
-          }),
-      );
-    }),
-  );
 }
